990b4cfd968b74edb7fdead58528602f815774ee
Jinchao Lin <jinchao.1926@gmail.com>
2026-06-26 14:11:30 +0800

    [SongURL] Add song url request

---
+++ SwiftSound/SwiftSoundTests/Request/Songs/SongPlaybackURLResponseTests.swift
@@ -0,0 +1,29 @@
+//
+//  SongPlaybackURLResponseTests.swift
+//  SwiftSoundTests
+//
+//  Created by Jinchao Lin on 2026/6/11.
+//
+
+import Testing
+import Foundation
+@testable import SwiftSound
+
+struct SongPlaybackURLResponseTests {
+
+    @Test func decodesSongPlaybackURLResponseFixture() throws {
+        let data = try SongPlaybackURLResponse.rawData()
+        let response = try JSONDecoder.swiftSoundDefault.decode(SongPlaybackURLResponse.self, from: data)
+        let playback = try #require(response.data.first)
+
+        #expect(response.code == 200)
+        #expect(response.data.count == 1)
+
+        #expect(playback.id == 64517)
+        #expect(!playback.url.isEmpty)
+        #expect(playback.type == "mp3")
+        #expect(playback.level == .standard)
+        #expect(playback.time == 298973)
+        #expect(playback.size == 4784840)
+    }
+}
